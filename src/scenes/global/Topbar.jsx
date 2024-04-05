1882b777a1ced3b98ce7d709a76cd505999d4ef4
duncanchiu409 <chiuduncan409@gmail.com>
2024-04-05 10:30:03 +0800

    day 4: theme setting through themeProvider

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,8 +1,30 @@
+import { Box, IconButton, useTheme } from '@mui/material'
+import { useContext } from 'react'
+import { ColorModeContext, tokens } from '../../theme'
+import { InputBase } from '@mui/material'
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+
 function Topbar(){
+    const theme = useTheme()
+    const colors = tokens(theme.palette.mode)
+    const colorMode = useContext(ColorModeContext)
+
     return (
-        <div>
-            Sidebar
-        </div>
+        <Box display="flex" justifyContent="space-between" p={2}>
+            <Box display="flex" backgroundColor={colors.primary[400]} borderRadius="3px">
+                <InputBase sx={{ ml:2, flex:1 }} placeholder="Search"/>
+                <IconButton type="button" sx={{ p:1 }}><SearchOutlinedIcon/></IconButton>
+            </Box>
+
+            <Box display="flex">
+
+            </Box>
+        </Box>
     )
 }
 
--- src/theme.js
+++ src/theme.js
@@ -198,13 +198,13 @@ export const ColorModeContext = createContext({
 })
 
 export const useMode = () => {
-    const [mode, setMode] = useState
+    const [mode, setMode] = useState('dark')
 
     const colorMode = useMemo(() => ({
         toggleColorMode: () => setMode(prev => prev === 'light' ? 'dark' : 'light')
     }), [])
 
-    const theme = useMemo(() => createTheme(themeSetting(mode)), [model])
+    const theme = useMemo(() => createTheme(themeSetting(mode)), [mode])
 
     return [theme, colorMode]
 }
