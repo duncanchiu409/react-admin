ce44485c412cecf008ca0f52dbd00ea4b7e3f61b
duncanchiu409 <chiuduncan409@gmail.com>
2024-04-12 18:03:28 +0800

    day 08: update sidebar

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,7 +1,14 @@
-function Dashboard(){
-    return (
-        <p>Dashboard is working</p>
-    )
+import Header from "../../components/Header";
+import { Box } from "@mui/material";
+
+function Dashboard() {
+  return (
+    <Box sx={{ m: "20px" }}>
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Header title="DASHBOARD" subtitle="WELCOME TO YOUR DASHBOARD"></Header>
+      </Box>
+    </Box>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
